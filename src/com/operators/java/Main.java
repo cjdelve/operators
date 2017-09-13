e9f10c62642c3d0fcc02965faf338d5677243dcb
Cameron Delve <cjdelve.cd@gmail.com>
2017-09-12 20:43:03 -0500

    Initial commit

---
+++ src/com/operators/java/Main.java
@@ -0,0 +1,27 @@
+package com.operators.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 1 + 2;
+        // result is now 3
+        System.out.println("1 + 2 = " + sum);
+
+        int product = 5 * 2;
+        // product = 10
+        System.out.println("5 * 2 = " + product);
+
+        int quotient = 14 / 2;
+        // quotient = 7
+        System.out.println("14 / 2 = " + quotient);
+
+        int remainder = 12 % 5;
+        // remainder = 2
+        System.out.println("12 % 4 = " + remainder);
+
+        int difference = 12 - 5;
+        // difference = 7
+        System.out.println("12 - 5 = " + difference);
+
+    }
+}
